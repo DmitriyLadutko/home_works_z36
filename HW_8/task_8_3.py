51632e787b2024daaa72c6eaaa66ad9c73fcc525
Antipositive <71846589+DmitriyLadutko@users.noreply.github.com>
2020-10-04 18:45:52 +0300

    Add files via upload

---
+++ HW_8/task_8_3.py
@@ -0,0 +1,16 @@
+def function_defining_palindrome(word_1, word_2, word_3):
+    if word_1 == word_1[:: -1]:
+        print('Является полиндромом')
+    else:
+        print('Не является полиндромом')
+    if word_2 == word_2[:: -1]:
+        print('Является полиндромом')
+    else:
+        print('Не является полиндромом')
+    if word_3 == word_3[:: -1]:
+        print('Является полиндромом')
+    else:
+        print('Не является полиндромом')
+
+
+function_defining_palindrome(word_1=input(), word_2=input(), word_3=input())
