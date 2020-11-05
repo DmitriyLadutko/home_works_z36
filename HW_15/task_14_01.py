09c45bcd8ac36e9bc7bf5ea6393cfea25f89a38c
Antipositive <71846589+DmitriyLadutko@users.noreply.github.com>
2020-11-05 14:14:29 +0300

    Add files via upload

---
+++ HW_15/task_14_01.py
@@ -0,0 +1,28 @@
+import csv
+import time
+from argparse import ArgumentParser
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument('-f', '--first_name', required=True)
+    parser.add_argument('-ln', '--last_name', required=True)
+    parser.add_argument('-hs', '--hours', required=True)
+    parser.add_argument('-m', '--minutes', required=True)
+    parser.add_argument('-s', '--seconds', required=True)
+    args = parser.parse_args()
+
+    list_ = [i for i in range(1+int(args.seconds))]
+    for i in reversed(list_):
+        print(f'{args.hours}:{args.minutes}:{i}')
+        time.sleep(1)
+    print('ALARM!!!')
+
+    new_file = 'Log.csv'
+    key_ = ['First name', 'last name', 'hours', 'minutes', 'seconds']
+
+    with open(new_file, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(key_)
+        writer.writerow([args.first_name, args.last_name, args.hours, args.minutes, args.seconds])
